bba02319140dbdceae5ad5a09d6d94f1a1e76f95
resatyildiz <resat328@gmail.com>
2023-11-24 10:03:05 +0300

    feat: create order status migration, seed and model

---
+++ database/seeders/OrderStatusSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class OrderStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\OrderStatus::factory()->create(
+            [
+                'name' => "Sipariş Alındı",
+                'slug' => "received",
+            ]
+        );
+        \App\Models\OrderStatus::factory()->create(
+            [
+                'name' => "Sipariş Tamamlandı",
+                'slug' => "completed",
+
+            ]
+        );
+        \App\Models\OrderStatus::factory()->create(
+            [
+                'name' => "Sipariş İptal Edildi",
+                'slug' => "canceled",
+            ]
+        );
+    }
+}
